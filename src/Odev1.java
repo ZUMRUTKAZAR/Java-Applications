4f47353e1e097e3db4925567a63b4eb4b2ecb61c
ZUMRUT123 <104307159+ZUMRUT123@users.noreply.github.com>
2022-09-17 17:16:52 +0300

    comment line is deleted on Odev 1

--- src/Odev1.java
+++ src/Odev1.java
@@ -1,11 +1,8 @@
 public class Odev1 {
 
     public static void main(String[] args) {
-System.out.println("Video5");
         System.out.println("Hello Java");
         System.out.println("Hello Java2");
-
-
 System.out.println("Video7");
 
 
