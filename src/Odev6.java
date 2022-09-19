b2684a182f5bc0530b28ebe81197709d5effe028
ZUMRUT123 <104307159+ZUMRUT123@users.noreply.github.com>
2022-09-19 17:31:05 +0300

    Odev6 class added

---
+++ src/Odev6.java
@@ -0,0 +1,9 @@
+public class Odev6 {
+    public static void main(String[] args) {
+        //for döngüsü ile çalışmak
+        for (int i = 20; i <= 55; i += 5) {
+            System.out.println(i);
+        }
+        System.out.println("|| DÖNGÜ BİTTİ ||");
+    }
+}
