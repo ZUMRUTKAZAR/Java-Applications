a445ed9b254f4aa235ed98811dd0179a729f0682
ZUMRUT123 <104307159+ZUMRUT123@users.noreply.github.com>
2022-09-21 18:25:33 +0300

    Odev13 class added

---
+++ src/Odev13.java
@@ -0,0 +1,18 @@
+public class Odev13 {
+    public static void main(String[] args) {
+        char harf='v';
+        switch (harf){
+            case'A':
+            case'B':
+            case'C':
+            case'D':
+                System.out.println("Kalın sesli harf.");
+                break;
+            default:
+                System.out.println("İnce sesli harf. ");
+
+
+
+        }
+    }
+}
